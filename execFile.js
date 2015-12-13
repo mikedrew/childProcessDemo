66c348f797ec7271e0e6e25518e9bf9a06fed621
Mike Drew <mike@optidatallc.com>
2015-12-13 18:31:52 -0500

    added -G for GPU mining

--- execFile.js
+++ execFile.js
@@ -5,7 +5,7 @@ var exec = require('child_process').execFile;
 var spawn = require('child_process').spawn;
 var child_process = require('child_process');
 
-var args = ['-F','http://ethereumpool.co/?miner=5@0x683feddafc2a8542744a4587de0c45626d7b8e68'];
+var args = ['-F','http://ethereumpool.co/?miner=5@0x683feddafc2a8542744a4587de0c45626d7b8e68','-G'];
 
 child_process.execFile('c:\\ethminer\\ethminer.exe',args,[],function (err, stdout, stderr) {
 	
